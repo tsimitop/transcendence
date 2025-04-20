75419fe5ea4ac61ab16eece374a96d51eb17aed0
Farshad <f.ahmadiyanyazdi@gmail.com>
2025-04-20 14:52:57 +0200

    style: style the Auth2Fa page

--- frontend/src/pages/Auth2Fa.ts
+++ frontend/src/pages/Auth2Fa.ts
@@ -15,6 +15,10 @@ class Auth2Fa extends Component {
 
     const main = document.createElement("main");
     main.classList.add(
+      "flex",
+      "flex-col",
+      "items-center",
+      "justify-center",
       "main-container",
       "layout-padding",
       `${
@@ -27,15 +31,17 @@ class Auth2Fa extends Component {
     main.addEventListener("click", Auth2Fa.handleClick);
 
     const html = `
-			<h1>Welcome ${userContext.state.username}</h1>
 			<div class="flex flex-col gap-8">
+				<h1 class="text-3xl">Hi <span class="font-bold">${userContext.state.username}</span></h1>
 				<form class="sign-in-form flex flex-col gap-3">
 					<div class="grid grid-cols-[150px_1fr] items-center">
-						<label for="2fa-code">6-digit code</label>
+						<label for="2fa-code">2FA code</label>
 						<input required type="number" name="2fa-code" id="2fa-code" placeholder="6-digit code" class="theme-input-${themeState.state} code-2fa-input w-80 px-2 py-1" />
 					</div>
-					<button type="submit" class="theme-btn-${themeState.state} code-2fa-btn cursor-pointer block ml-auto mr-0 px-6 py-2">Submit</button>
-					<button type="submit" class="theme-btn-${themeState.state} cancel-2fa-btn cursor-pointer block ml-auto mr-0 px-6 py-2">Cancel</button>
+					<div class="flex justify-end gap-3">
+					<button type="submit" class="theme-btn-${themeState.state} cancel-2fa-btn cursor-pointer block px-6 py-2 w-[90px] flex justify-center">Cancel</button>
+						<button type="submit" class="theme-btn-${themeState.state} code-2fa-btn cursor-pointer block px-6 py-2 w-[90px] flex justify-center">Submit</button>
+					</div>
 				</form>
 			</div>
 			`;
