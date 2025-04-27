55591a118be51133a889fc0492e0065ec9f19adb
f321x <f321x@tutamail.com>
2025-04-27 21:43:05 +0200

    enable frontend dev server

--- docker/frontend-dev/frontend-entry.sh
+++ docker/frontend-dev/frontend-entry.sh
@@ -2,13 +2,11 @@
 
 cd /app
 
-# npm install -g --verbose typescript@5.8.3
-
 npm ci --verbose
 
 npm run build
 
 # just exit again after the build has finished.
 # Now caddy can take the files and serve them.
-echo "Frontend build finished. Start caddy server now. "
-# exec npm run host
+echo "Frontend build finished."
+exec npm run host
